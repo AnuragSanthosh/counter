e27c21dc474944e0706055e60a095b02e7a06f12
AnuragSanthosh <kvsasd@gmail.com>
2024-04-30 11:38:01 +0530

    commit

--- Server/App.js
+++ Server/App.js
@@ -4,13 +4,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
+const PORT = 5000;
+require('dotenv').config();
 app.use(express.json());
 app.use(cors());
 
 // MongoDB Connection
-mongoose.connect(connection_url)
+mongoose.connect(process.env.connection_url)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Error connecting to MongoDB:', err));
 
